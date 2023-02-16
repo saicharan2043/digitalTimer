abe074ec9a844d2b64a4ed1c76044ca9df62010c
Saicharan2043 <saikanna2043@gmail.com>
2023-02-16 21:44:04 +0000

    save code

---
+++ src/components/DigitalTimer/index.js
@@ -0,0 +1,49 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class DigitalTimer extends Component {
+  state = {}
+
+  render() {
+    return (
+      <div className="">
+        <h1 className="">Digital Timer</h1>
+        <div className="">
+          <div className="">
+            <h1 className="">25:00</h1>
+            <p className="">running</p>
+          </div>
+        </div>
+
+        <div className="">
+          <div className="">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/play-icon-img.png"
+              alt=""
+              className=""
+            />
+            <p className="">start</p>
+          </div>
+
+          <div className="">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/reset-icon-img.png"
+              alt=""
+              className=""
+            />
+            <p className="">Reset</p>
+          </div>
+          <p className="">Set Timer limit</p>
+          <div className="">
+            <button className="">-</button>
+            <button className="">25</button>
+            <button className="">+</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default DigitalTimer
